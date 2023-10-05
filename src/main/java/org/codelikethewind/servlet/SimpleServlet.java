851db2983a1c74a2cd863e8ed51da04c8b051e91
tipu-zlatan <muhammadbinazhar2@gmail.com>
2023-10-05 16:47:08 +0300

    built-5

--- src/main/java/org/codelikethewind/servlet/SimpleServlet.java
+++ src/main/java/org/codelikethewind/servlet/SimpleServlet.java
@@ -21,7 +21,7 @@ public class SimpleServlet extends HttpServlet {
 		PrintWriter out = response.getWriter();
 		out.println("<html>");
 		out.println("<body>");
-		out.println("<h1>" + "Mello Lumen qar11111" + "</h1>");
+		out.println("<h1>" + "Mello Lumen qar11---" + "</h1>");
 		out.println("</body>");
 		out.println("</html>");
 	}
