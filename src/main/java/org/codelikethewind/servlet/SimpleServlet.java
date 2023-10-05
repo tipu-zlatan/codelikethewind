4008b04a477cfc4f5f375fdec747ed80dcf11e12
tipu-zlatan <64974141+tipu-zlatan@users.noreply.github.com>
2023-10-05 16:12:57 +0300

    Update SimpleServlet.java

--- src/main/java/org/codelikethewind/servlet/SimpleServlet.java
+++ src/main/java/org/codelikethewind/servlet/SimpleServlet.java
@@ -21,7 +21,7 @@ public class SimpleServlet extends HttpServlet {
 		PrintWriter out = response.getWriter();
 		out.println("<html>");
 		out.println("<body>");
-		out.println("<h1>" + "Hello Lumen qar" + "</h1>");
+		out.println("<h1>" + "Hello Lumen qar1" + "</h1>");
 		out.println("</body>");
 		out.println("</html>");
 	}
